21919cc0003097fcace04d373f2e4b1fe5d031c6
Lina Salinas <linasalinasd7@gmail.com>
2019-05-14 17:36:52 -0500

    First version of the software project. AcademicUnit class created with part of the binary search tree

---
+++ src/model/AcademicUnit.java
@@ -0,0 +1,61 @@
+package model;
+
+public class AcademicUnit implements Comparable{
+	
+	private String name;
+	private  int code;
+	private String director;
+	private AcademicUnit right;
+	private AcademicUnit left;
+	
+	public AcademicUnit(String name, int code, String director) {
+
+		this.name = name;
+		this.code = code;
+		this.director = director;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public AcademicUnit getRight() {
+		return right;
+	}
+
+	public void setRight(AcademicUnit right) {
+		this.right = right;
+	}
+
+	public AcademicUnit getLeft() {
+		return left;
+	}
+
+	public void setLeft(AcademicUnit left) {
+		this.left = left;
+	}
+
+	public String getDirector() {
+		return director;
+	}
+
+	@Override
+	public int compareTo(Object arg0) {
+		
+		return 0;
+	}
+
+
+}
